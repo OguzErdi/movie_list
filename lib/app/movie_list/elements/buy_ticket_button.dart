94aaf7dd8b1732926c38bddb9ccd0349326dcd52
OguzErdi <erdi.erkmen@gmail.com>
2020-09-07 23:59:23 +0300

    file rename

---
+++ lib/app/movie_list/elements/buy_ticket_button.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class BuyTicketButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(bottom: 20),
+      child: ButtonTheme(
+        minWidth: 230,
+        height: 50,
+        child: RaisedButton(
+          color: Colors.grey[850],
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+          onPressed: () {},
+          child: Text(
+            "BUY TICKET",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 16
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
